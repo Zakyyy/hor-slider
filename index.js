1267b9034a8c3a72de0da53c6a6a7d7d8fa1869d
Zakyyy <ahmed.abdelghafour@student.guc.edu.eg>
2020-04-22 16:59:51 +0200

    added documentation

--- index.js
+++ index.js
@@ -3,21 +3,36 @@ import { FlatList, TouchableOpacity, Image, View, Text, StyleSheet, Alert } from
 import PropTypes from 'prop-types'
 import Star from 'react-native-star-view';
 export default class HorizontalSlider extends React.Component {
+  // how to render each item in the array of data.
   renderItem({ item }) {
     return (
+      /*
+        The main wrapper component is touchable opacity which is a button so that,
+        when the user presses on any part of the item it shows the alert with the image url
+       */
       <TouchableOpacity
         key={item.id}
         style={styles.buttonStyle}
-        onPress={() => Alert.alert('', `${item.image.uri}`)}
+        // if the image url doesnot exist show just a url word
+        onPress={() => Alert.alert('', item.image.uri ? `${item.image.uri}` : 'URL')}
       >
+        {/* image component to show the listing image */}
         <Image
-          source={item.image}
+          source={item.image ? item.image : null}
           style={styles.imageStyle}
         />
+        {/* View component the wraps all the remaining listing data */}
         <View style={styles.dataContainer}>
-          <Text style={styles.categoryStyle}>{item.category}</Text>
-          <Star score={item.rating} style={styles.starStyle} />
-          <Text style={styles.titleStyle} >{item.title}</Text>
+          {/* 
+          before rendering any attribute, check first if exists and then show it other than that there
+          are two options;
+          first one to add a condition on the whole component so that not to render it if the attribute
+          doesnot exist.
+          second option as it is implemented down here to show a default value if it doesnot exist and prevents app crashing
+          */}
+          <Text style={styles.categoryStyle}>{item.category ? item.category : ''}</Text>
+          <Star score={item.rating ? item.rating : 0} style={styles.starStyle} />
+          <Text style={styles.titleStyle}>{item.title ? item.title : ''}</Text>
 
         </View>
       </TouchableOpacity>
@@ -25,7 +40,9 @@ export default class HorizontalSlider extends React.Component {
   }
   render() {
     return (
+      // using the traditional react native flat list and specifying the needed props
       <FlatList
+        // the all props here to give the package's user the ability to pass further more flatlist props
         {...this.props}
         data={this.props.data}
         keyExtractor={({ id }) => id}
@@ -37,6 +54,14 @@ export default class HorizontalSlider extends React.Component {
   }
 }
 
+/* using propTypes package to warn the package's user about some props and
+ here stating that the data prop is required in order to use the package */
+HorizontalSlider.propTypes = {
+  data: PropTypes.array.isRequired
+};
+
+
+// styles object responsible for the component's styling
 const styles = StyleSheet.create({
   titleStyle: {
     fontSize: 16, color: 'black', fontWeight: 'bold', marginTop: 5
@@ -60,6 +85,4 @@ const styles = StyleSheet.create({
   }
 })
 
-HorizontalSlider.propTypes = {
-  data: PropTypes.array.isRequired
-};
+
